0ad8a2ce99ea9d338c449361ba3eb93b5cf76e1b
meysam-pc <instasakoo@gmail.com>
2024-07-30 18:41:13 +0330

    property filters 90%

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -31,18 +31,39 @@ class Property extends Model
     // {
     //     return $this->belongsTo(City::class, 'city_id');
     // }
-    
+
     public function scopeFilter($query)
     {
+
+        if (request()->has('furnish')) {
+            $furnish = explode(',', request()->furnish,);
+            $query->whereIn('furnish', $furnish);
+        }
+        if (request()->has('inserter_role')) {
+            $inserter_role = explode(',', request()->inserter_role,);
+            $query->whereIn('inserter_role', $inserter_role);
+        }
+        if (request()->has('place_type')) {
+            $place_type = explode(',', request()->place_type,);
+            $query->whereIn('place_type', $place_type);
+        }
         if (request()->has('bedrooms')) {
-            $bedrooms = explode(',',request()->bedrooms,);
-            // dd($bedrooms);
+            $bedrooms = explode(',', request()->bedrooms,);
+
             $query->whereIn('bedrooms', $bedrooms);
         }
-        
+        if (request()->has('bathrooms')) {
+            $bathrooms = explode(',', request()->bathrooms,);
+            $query->whereIn('bathrooms', $bathrooms);
+        }
+
         if (request()->has('plusbedrooms')) {
             $plusbedrooms = request()->plusbedrooms;
-            $query->where('bedrooms', '>', $plusbedrooms);
+            $query->where('bedrooms', '>=', $plusbedrooms);
+        }
+        if (request()->has('pet_friendly')) {
+            $pet_friendly = request()->pet_friendly;
+            $query->where('pet_friendly',  $pet_friendly);
         }
         if (request()->has('category_id')) {
             $category_id = request()->category_id;
@@ -52,14 +73,25 @@ class Property extends Model
             $type = request()->type;
             $query->where('type', $type);
         }
+
         if (request()->has('max_price')) {
             $max_price = request()->max_price;
-            $query->where('price', '<', $max_price);
+            $query->where('price', '<=', $max_price);
         }
         if (request()->has('min_price')) {
             $min_price = request()->min_price;
-            $query->where('price', '>', $min_price);
+            $query->where('price', '>=', $min_price);
         }
+
+        if (request()->has('max_rent')) {
+            $max_rent = request()->max_rent;
+            $query->where('monthly_rent', '<=', $max_rent);
+        }
+        if (request()->has('min_rent')) {
+            $min_rent = request()->min_rent;
+            $query->where('monthly_rent', '>=', $min_rent);
+        }
+
         if (request()->has('min_size')) {
             $min_size = request()->min_size;
             $query->where('size', '>', $min_size);
@@ -71,6 +103,10 @@ class Property extends Model
         if (request()->has('provider')) {
             $provider = request()->provider;
             $query->where('inserter_role', $provider);
+        }
+        if (request()->has('rental_period')) {
+            $rental_period = request()->rental_period;
+            $query->where('rental_period', $rental_period);
         }
     }
 }
